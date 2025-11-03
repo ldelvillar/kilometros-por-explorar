25b9bc35d31d6a48f049e7c65dc1317127b9cdbe
ldelvillar <ldelvillarvazquez@gmail.com>
2025-11-03 17:59:38 +0100

    refactor: rename clientes to viajeros with redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,10 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.kilometrosporexplorar.es',
+  redirects: {
+    '/clientes': '/viajeros',
+    '/clientes/[slug]': '/viajeros/[slug]',
+  },
   vite: {
     plugins: [tailwindcss()],
   },
